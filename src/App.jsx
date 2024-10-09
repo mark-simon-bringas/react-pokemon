66b1bed58bc13395536b35699133057ec5146477
bringas_m <bringas_m@f112.lan>
2024-10-09 11:47:09 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Header from './Header'
+import Footer from './Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  )
+}
+
+export default App
