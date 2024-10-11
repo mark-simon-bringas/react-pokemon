4b2688151b7894ac07fbfb35e8b827590463655b
Mark Simon <msimonbringas05v2@gmail.com>
2024-10-11 21:37:44 +0800

    Added live search functionality

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ export default function Header({ input, search, region }) {
     return (
         <>
             <header>
-                <h1>PokeDex</h1>
+                <h1>PokéDex</h1>
                 <div className="search-bar">
                     <input type="text" onChange={input} placeholder="Search Pokemon" />
                     <button onClick={search}>Search</button>
