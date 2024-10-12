ecdc4ad7e768cb62060002557f124b38eb4a2989
Mark Simon <msimonbringas05v2@gmail.com>
2024-10-12 23:29:27 +0800

    Added routing of pokemon description

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,24 @@
+import { Link } from 'react-router-dom';
 import Filters from "./Filters";
 
 export default function Header({ input, search, region }) {
     return (
         <>
             <header>
-                <h1>PokéDex</h1>
+                <Link to="/" style={{textDecoration: 'none'}}>
+                    <h1>PokéDex</h1>
+                </Link>
                 <div className="search-bar">
-                    <input type="text" onChange={input} placeholder="Search Pokemon" />
+                    <input 
+                        type="text" 
+                        onChange={input} 
+                        placeholder="Search Pokemon" 
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter') {
+                                search();
+                            }
+                        }}
+                    />
                     <button onClick={search}>Search</button>
                     <Filters onRegionChange={region} />
                 </div>
